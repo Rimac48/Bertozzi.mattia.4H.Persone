ddbe8d54fbda22ebca33abf5694002e52c0d14ab
Rimac48 <mattia.bertozzi@live>
2021-01-20 10:14:21 +0100

    Quinto Commit

---
+++ Bertozzi.mattia.4H.Persone/Models/Persona.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bertozzi.mattia._4H.Persone.Models
+{
+    class Persona
+    {
+        private string _cognome;
+        private string _nome;
+        private int _eta;
+
+        public string Cognome
+        {
+            get
+            {
+                return _cognome;
+            }
+            set
+            {
+                _cognome = value; 
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public int Eta
+        {
+            get
+            {
+                return _eta;
+            }
+            set
+            {
+                _eta = value;
+            }
+        }
+
+        public string VediNome()
+        {
+            return $"Nome:{_nome}";
+        }
+
+        public string VediCognome()
+        {
+            return $"Cognome:{_cognome}";
+        }
+
+        public string VediEta()
+        {
+            return $"Eta:{_eta}";
+        }
+
+        public void ModificaEta()
+        {
+           Console.WriteLine("inserisci la nuova Eta");
+            string streta = Console.ReadLine();
+            int eta = Convert.ToInt32(streta);
+            _eta = eta;
+        }
+
+        public string StampaAnagrafica()
+        {
+            return $"Dati anagrafici: \n \tCognome:{_cognome} \n \tNome:{_nome} \n \tEta:{_eta}";
+        }
+
+
+        //PER CONFRONTI
+        public static bool operator > (Persona p,Persona p2)
+        {
+            if(p.Eta > p2.Eta)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(Persona p, Persona p2)
+        {
+            if (p.Eta > p2.Eta)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool operator == (Persona p, Persona p2)
+        {
+            if (p.Eta == p2.Eta)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Persona p, Persona p2)
+        {
+            if (p.Eta != p2.Eta)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Bertozzi.mattia.4H.Persone/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Bertozzi.mattia._4H.Persone.Models;
+
+namespace Bertozzi.mattia._4H.Persone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programma Persone di Mattia Bertozzi");
+
+            //persone disponibili nel vettore 3
+            Persona People = new Persona();
+
+            Persona People2 = new Persona();
+
+            Console.WriteLine("Inserire cognome 1a persona");
+            People.Cognome = Console.ReadLine();
+            Console.WriteLine("Inserire nome 1a persona");
+            People.Nome = Console.ReadLine();
+            Console.WriteLine("Inserire eta 1a persona");
+            string streta = Console.ReadLine();
+            int eta = Convert.ToInt32(streta);
+            People.Eta = eta;
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Inserire cognome 2a persona");
+            People2.Cognome = Console.ReadLine();
+            Console.WriteLine("Inserire nome 2a persona");
+            People2.Nome = Console.ReadLine();
+            Console.WriteLine("Inserire eta 2a persona");
+            string streta2 = Console.ReadLine();
+            int eta2 = Convert.ToInt32(streta2);
+            People2.Eta = eta2;
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Dati Anagrafici eta 1a persona");
+            Console.WriteLine(People.StampaAnagrafica());
+            Console.WriteLine("\n");
+            Console.WriteLine("Dati Anagrafici eta 2a persona");
+            Console.WriteLine(People2.StampaAnagrafica());
+
+            Console.WriteLine("\n");
+
+            //per confornto
+            if (People==People2)
+            {
+                Console.WriteLine("Le 2 persone sono coetanee");
+            }
+            else if (People > People2)
+            {
+                Console.WriteLine("La persona:");
+                Console.WriteLine("\n");
+                Console.WriteLine(People2.StampaAnagrafica());
+                Console.WriteLine("\n");
+                Console.WriteLine("è più giovane");
+            }
+            else if (People < People2)
+            {
+                Console.WriteLine("La persona:");
+                Console.WriteLine(People.StampaAnagrafica());
+                Console.WriteLine("è più giovane");
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Vuoi modificare l'eta della '1'a o '2'a persona?");
+            string scelta = Console.ReadLine();
+            if(scelta=="1")
+            {
+                People.ModificaEta();
+            }
+            else if(scelta == "2")
+            {
+                People2.ModificaEta();
+            }
+
+            Console.WriteLine("Eta Modificata");
+
+        }
+    }
+}
